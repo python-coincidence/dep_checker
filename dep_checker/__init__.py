cef7ecd6de53ea63f35382eb3b61f2937cabf436
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-09 13:30:59 +0000

    Bump version v0.0.0 -> v0.1.0

--- dep_checker/__init__.py
+++ dep_checker/__init__.py
@@ -44,7 +44,7 @@ from stdlib_list import stdlib_list
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.0.0"
+__version__: str = "0.1.0"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = ["template", "check_imports"]
