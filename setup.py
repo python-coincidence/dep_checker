fec25aaa6284d1d0b89e365e7ff250e984a2ac77
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-20 21:19:24 +0000

    Updated config files.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ sys.path.append('.')
 from __pkginfo__ import *  # pylint: disable=wildcard-import
 
 setup(
-		description='Tool to check all requirements are actually required.',
+		description="Tool to check all requirements are actually required.",
 		extras_require=extras_require,
 		install_requires=install_requires,
 		py_modules=[],
